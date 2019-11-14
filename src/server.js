4273ef47a0b5fa99f56940a348e1de89a2f4334a
Jeff Lapp <lappjeff20@gmail.com>
2019-11-14 11:46:30 -0500

    Created demo subroute

--- src/server.js
+++ src/server.js
@@ -5,17 +5,24 @@ import cors from 'cors'
 
 export const app = express()
 
+const router = express.Router()
+
 app.disable('x-powered-by')
 
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use('/api', router)
 
 const log = (req, res, next) => {
   console.log('logging')
   next()
 }
+
+router.get('/me', (req, res) => {
+  res.send({ me: 'jeff' })
+})
 
 app.get('/data', [log, log], (req, res) => {
   res.send({ message: 'hello' })
